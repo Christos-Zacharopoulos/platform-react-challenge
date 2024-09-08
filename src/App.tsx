e7de0a7684977ef90e1070d25b58e7aa751750a8
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2024-09-08 18:09:35 +0300

    feat: app routing strategy and views

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Breeds from "./pages/Breeds";
+import Favorites from "./pages/Favorites";
+
 const App = () => {
   return (
-    <>
-      <p className="text-3xl font-bold underline">Hello World</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Home with cat modal */}
+        <Route path="/cat/:catId" element={<Home />} />
+        <Route path="/breeds" element={<Breeds />} />
+        {/* Breeds with breed modal */}
+        <Route path="/breeds/:breedId" element={<Breeds />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/pages/Breeds/Breeds.tsx
@@ -0,0 +1,5 @@
+const Breeds = () => {
+  return <h1>Breeds</h1>;
+};
+
+export default Breeds;
