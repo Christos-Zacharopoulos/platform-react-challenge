b8ee072f1a508a3d2c249deab0b6f0d0d492788f
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2024-09-09 20:04:45 +0300

    feat: img and modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import ReactDOM from "react-dom";
+import { PropsWithChildren } from "react";
+import Button from "../Button";
+
+export type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const Modal = ({
+  isOpen,
+  onClose,
+  children,
+}: PropsWithChildren<ModalProps>) => {
+  const root = document.getElementById("modal-root");
+
+  if (!isOpen || !root) return null;
+
+  return ReactDOM.createPortal(
+    <div
+      className="fixed top-0 left-0 right-0 bottom-0  bg-opacity-70 bg-slate-500 flex justify-center items-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className=" bg-gray-800 p-3 max-w-screen-xl w-full  rounded-md relative"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="absolute top-4 right-4 ">
+          <Button onClick={onClose}>Close</Button>
+        </div>
+        {children}
+      </div>
+    </div>,
+    root
+  );
+};
+
+export default Modal;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
-import api from "../../api";
-import { Image } from "../../api/images/types";
-import Button from "../../components/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import Img from "../../components/Img";
 import List from "../../components/List";
 import ListItem from "../../components/List/components/ListItem";
+import Button from "../../components/Button";
+import api from "../../api";
+import { Image } from "../../api/images/types";
+import Modal from "../../components/Modal";
 
 const Home = () => {
   const [cats, setCats] = useState<Image[]>([]);
+  const [modalCatOpen, setModalCatOpen] = useState(false);
+  const navigate = useNavigate();
+  const { catId } = useParams();
 
   useEffect(() => {
     fetchRandomCats();
@@ -18,27 +24,36 @@ const Home = () => {
     setCats((prevCats) => [...prevCats, ...data]);
   };
 
+  const openModal = (cat: Image) => {
+    setModalCatOpen(true);
+    navigate(`/cat/${cat.id}`);
+  };
+
+  const closeModal = () => {
+    setModalCatOpen(false);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (catId) {
+      setModalCatOpen(true);
+      navigate(`/cat/${catId}`);
+    }
+  }, [catId]);
+
   return (
     <div>
       <List>
         {cats.map((cat) => (
-          <ListItem key={cat.id} onClick={() => console.log(cat)}>
-            <img
-              src={cat.url}
-              alt="random cat"
-              style={{
-                width: "200px",
-                height: "200px",
-                margin: "10px",
-                cursor: "pointer",
-              }}
-            />{" "}
+          <ListItem key={cat.id} onClick={() => openModal(cat)}>
+            <Img src={cat.url} alt="random cat" />
           </ListItem>
         ))}
       </List>
       <div className="flex justify-center p-3">
         <Button onClick={fetchRandomCats}>Load More</Button>
       </div>
+      <Modal isOpen={modalCatOpen} onClose={closeModal} />
     </div>
   );
 };
